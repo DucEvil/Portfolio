eaa6bebbda829a3bc5bc24061bfeed4f51b46a1b
Đỗ Đức <ducdodinhhz18@gmail.com>
2025-11-17 20:27:04 +0700

    dasua

--- projects/script.js
+++ projects/script.js
@@ -20,8 +20,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
-            $("#favicon").attr("href", "/assets/images/favicon.png");
+            document.title = "Projects | Portfolio Đỗ Đình Đức";
+            $("#favicon").attr("href", "/assets/images/avt.png");
         }
         else {
             document.title = "Come Back To Portfolio";
